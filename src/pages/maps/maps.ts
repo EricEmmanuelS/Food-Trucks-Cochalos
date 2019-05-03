99a4f18a5be34129463a9a84ea9001aedec319ca
Eric Galleuillos <ericgaso97@gmail.com>
2019-05-02 22:39:18 -0400

    prueba
    prueba

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -10,97 +10,59 @@ declare var google;
   templateUrl: 'maps.html',
 })
 export class MapsPage {
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
   map: any;
-  directionsService: any = null;
-  directionsDisplay: any = null;
-  bounds: any = null;
-  myLatLng: any;
-  waypoints: any[];
+  originPosition: string;
+  destinationPosition: string;
 
-  constructor(
-    public navCtrl: NavController,
-    public geolocation: Geolocation
-  ) {
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsDisplay = new google.maps.DirectionsRenderer();
-    this.bounds = new google.maps.LatLngBounds();
+  constructor(private geolocation: Geolocation) { }
 
-    this.waypoints = [
-      {
-        location: { lat: -17.3339072, lng: -66.2254597 },
-        stopover: true,
-      },
-      {
-        location: { lat: -17.3339072, lng: -66.2254597 },
-        stopover: true,
-      }
-];
+  ionViewDidLoad() {
+    this.initializeMap();
   }
 
-  ionViewDidLoad(){
-    this.getPosition();
-  }
-
-  getPosition():any{
+  
+  initializeMap() {
     this.geolocation.getCurrentPosition()
-    .then(response => {
-      this.loadMap(response);
-    })
-    .catch(error =>{
-      console.log(error);
-    })
-  }
+    .then((resp) => {
+      this.originPosition = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      this.destinationPosition = -17.3339072 + "," + -66.2254597
 
-  loadMap(position: Geoposition){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    console.log(latitude, longitude);
-    // create a new map by passing HTMLElement
-    let mapEle: HTMLElement = document.getElementById('map');
-
-    // create LatLng object
-    this.myLatLng = {lat: latitude, lng: longitude};
-
-    // create map
-    this.map = new google.maps.Map(mapEle, {
-      center: this.myLatLng,
-      zoom: 12
-    });
-
-    this.directionsDisplay.setMap(this.map);
-
-    google.maps.event.addListenerOnce(this.map, 'idle', () => {
-      mapEle.classList.add('show-map');
-      this.calculateRoute();
-    });
-  }
-
-  private calculateRoute(){
-    
-    this.bounds.extend(this.myLatLng);
-
-    this.waypoints.forEach(waypoint => {
-      var point = new google.maps.LatLng(waypoint.location.lat, waypoint.location.lng);
-      this.bounds.extend(point);
-    });
-
-    this.map.fitBounds(this.bounds);
-
-    this.directionsService.route({
-      origin: new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng),
-      destination: new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng),
-      waypoints: this.waypoints,
-      optimizeWaypoints: true,
-      travelMode: google.maps.TravelMode.DRIVING,
-      avoidTolls: true
-    }, (response, status)=> {
-      if(status === google.maps.DirectionsStatus.OK) {
-        console.log(response);
-        this.directionsDisplay.setDirections(response);
-      }else{
-        alert('Could not display directions due to: ' + status);
+      const mapOptions = {
+        zoom: 18,
+        center: this.originPosition
       }
-    });  
 
+      this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+      const marker = new google.maps.Marker({
+        position: this.originPosition,
+        map: this.map
+      });
+
+    }).catch((error) => {
+      console.log('Error al Recuperar la Posicion', error);
+    });
+  }
+
+  calculateRoute() {
+    if (this.destinationPosition && this.originPosition) {
+      const request = {
+        origin: this.originPosition,
+        destination: this.destinationPosition,
+        travelMode: 'DRIVING'
+      };
+
+      this.traceRoute(this.directionsService, this.directionsDisplay, request);
+    }
+  }
+
+  traceRoute(service: any, display: any, request: any) {
+    service.route(request, function (result, status) {
+      if (status == 'OK') {
+        display.setDirections(result);
+      }
+    });
   }
 }
